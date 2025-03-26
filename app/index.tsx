3e2b78566a2b05afc3139f745660caf685e9b865
Amardeep Ranjan <amardeepranjan911@gmail.com>
2025-03-26 22:13:37 +0530

    intial commmit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-900">
+      <Text className="text-2xl text-red-500 font-bold">Hello world</Text>
+    </View>
+  );
+}
